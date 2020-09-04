7c20ce6088f6590b6558452e3134effc412aac0b
ngwoon <todory2002@gmail.com>
2020-09-04 23:07:20 +0900

    random-quote

---
+++ Random-Quote/index.js
@@ -0,0 +1,46 @@
+function makeBtn() {
+    const btnDiv = document.querySelector(".menu");
+    const btn = document.createElement("input");
+    btn.type = "button";
+    btn.value = "Other Quotes";
+    btn.classList.add("button");
+    btn.addEventListener("click", function() {
+        location.reload();
+    });
+    btnDiv.appendChild(btn);
+}
+
+function getQuote() {
+    fetch("https://api.quotable.io/random").then(response => response.json())
+    .then(data => {
+        const quote = document.querySelector(".js-quote");
+        quote.innerHTML = `
+            <hr/>
+            <div class="quote-content">
+                ${data.content}
+            </div>
+            <br/>
+            <div class="quote-author">
+                by ${data.author}
+            </div>
+            <hr/>
+        `;
+
+        makeBtn();
+    });
+}
+
+function init() {
+    fetch("https://api.unsplash.com/photos/random/?client_id=bS6Mcj574RjNJEwIEl81OVOBfSJjaEMIvBTnYpRLmWE&query=light").then(response => response.json())
+    .then(json => {
+        const bgImg = new Image();
+        bgImg.classList.add("bgImage");
+        bgImg.src = json.urls.full;
+        bgImg.onload = function() {
+            document.body.prepend(bgImg);
+            getQuote();
+        };
+    });
+}
+
+init();
